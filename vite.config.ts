0b063be9e2cdbe538deb5c3223840fb872eae921
Ella Yeonsu Choi <happymondaysu@gmail.com>
2024-02-19 23:32:24 +0900

    build: Update vite.config.ts to successful build

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  root: "src",
   plugins: [svgr(), react()],
-  build: {
-    outDir: "../public",
-  },
 })
