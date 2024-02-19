8b48703d72d92197d5e97158a704d430f8c071bc
Ella Yeonsu Choi <happymondaysu@gmail.com>
2024-02-19 18:42:57 +0900

    feat: Update the Title to Navigate to the Main Page When the Logo is Clicked

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -1,11 +1,14 @@
-import TitleStyle from './Title.style';
+import { TitleStyle } from './Title.style';
+import { useNavigate } from 'react-router';
 
 const Title = () => {
+  const navigate = useNavigate();
+
   return (
-    <TitleStyle>
+    <TitleStyle onClick={() => navigate('/')}>
       🔀 랜덤 피커
     </TitleStyle>
-  )
+  );
 }
 
 export default Title;
