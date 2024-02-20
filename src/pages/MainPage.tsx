41f40fab66892bde13b578c032bb121e055b1202
Ella Yeonsu Choi <happymondaysu@gmail.com>
2024-02-20 16:06:01 +0900

    feat: Add a Title Image, Followed by an Option for Dark Mode or Light Mode

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,15 +1,26 @@
 import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 import { MainPageProps } from '../types/pages.type'
 
+import { useTheme } from '../contexts/ThemeProvider';
 import Toggle from '../components/Toggle/Toggle';
 import Title from '../components/Title/Title';
 import { PrimaryButton, NavigationButton } from '../components/Button/Button';
+import TitleImageinDarkMode from '../../public/TitleImage-white.png';
+import TitleImageinLightMode from '../../public/TitleImage-black.png';
 
-import { Container, ButtonsContainer } from '../components/Container/Container.style';
+import { Container, ButtonsContainer, ImageContainer } from '../components/Container/Container.style';
 
 const MainPage: React.FC<MainPageProps> = ({ participants }) => {
+
   const navigate = useNavigate();
+  const { isDarkMode, setIsDarkMode } = useTheme();
+
+  useEffect(() => {
+    const currentTheme = document.body.classList.contains('dark-mode') ? true : false;
+    setIsDarkMode(currentTheme);
+  }, [setIsDarkMode]);
 
   const handleSetting = () => {
     if (participants.length === 0) {
@@ -22,8 +33,18 @@ const MainPage: React.FC<MainPageProps> = ({ participants }) => {
 
   return (
     <Container>
-      <Toggle/>
+      <Toggle
+        isDarkMode={isDarkMode}
+        setIsDarkMode={setIsDarkMode}
+      />
       <Title/>
+      <ImageContainer>
+        {isDarkMode ? (
+          <img src={TitleImageinDarkMode} width="300rem" alt="Title Image in Dark Mode"/>
+        ) : (
+          <img src={TitleImageinLightMode} width="300rem" alt="Title Image in Light Mode"/>
+        )}
+      </ImageContainer>
       <ButtonsContainer gap='1rem'>
         <PrimaryButton to='/setting'>참여자 설정</PrimaryButton>
         <NavigationButton onClick={handleSetting}>당첨자 뽑기</NavigationButton>
