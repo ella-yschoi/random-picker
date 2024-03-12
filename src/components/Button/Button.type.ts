cf155c85c5745ca74dbee3e4dd201f7c6fa46558
Ella Yeonsu Choi <happymondaysu@gmail.com>
2024-03-12 16:44:18 +0900

    Delete src/components/Button directory

--- src/components/Button/Button.type.ts
+++
@@ -1,9 +0,0 @@
-import { MouseEventHandler } from 'react';
-
-interface ButtonProps {
-  children: React.ReactNode;
-  to?: string;
-  onClick?: MouseEventHandler<HTMLDivElement>;
-}
-
-export type { ButtonProps };
