f80e0e3c7327c2720650cef3f4eaaf210a74ad0f
Ella Yeonsu Choi <happymondaysu@gmail.com>
2024-03-12 17:00:12 +0900

    style: Apply Pointer Cursor to TitleStyle

--- src/components/title/Title.style.ts
+++ src/components/title/Title.style.ts
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled';
 
 const TitleStyle = styled.div`
+  cursor: pointer;
   margin: 1.7rem 0rem 0rem 0rem;
   font-size: 3.3rem;
   font-family: 'Pretendard-Black';
